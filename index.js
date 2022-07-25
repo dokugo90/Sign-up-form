e850a2a8729c185e1f50ad970266273e06cb49a6
dokugo90 <dokugo90@gmail.com>
2022-07-25 23:48:28 +0000

    git push

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function validate_password () {
           document.getElementById('create').disabled = true;
           document.getElementById('create').style.opacity = (0.4);
     }  else {
-        /*document.getElementById('wrong_pass_alert').style.color = 'green';
+        /* document.getElementById('wrong_pass_alert').style.color = 'green';
         document.getElementById('wrong_pass_alert').innerHTML =
             '🗹 Password Matched'; */
         document.getElementById('create').disabled = false;
